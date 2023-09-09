b43199e5fba249a6de6cb65416a50bc2b5031000
ViktorKrGo <526479@student.saxion.nl>
2023-09-09 19:29:49 +0200

    Changed some method returns.

--- src/LinkedList/IList.java
+++ src/LinkedList/IList.java
@@ -2,13 +2,13 @@ package LinkedList;
 
 public interface IList<T> {
     boolean add(T element);
-    boolean add(T element, int index);
-    boolean get(int index);
+    void add(T element, int index);
+    Node<T> get(int index);
     int indexOf(T element);
     boolean contains(T element);
     boolean set(int index, T element);
-    boolean remove(int index);
-    boolean remove(T element);
+    Node<T> remove(int index);
+    Node<T> remove(T element);
     void clear();
     boolean isEmpty();
     int size();
