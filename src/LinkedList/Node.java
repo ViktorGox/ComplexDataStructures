31b725b16debc6924bb3752c7574ac02442bd699
ViktorKrGo <526479@student.saxion.nl>
2023-09-10 12:05:38 +0200

    Added previous Node variable to Node class, and made the toString print it, the previous is always null currently though.

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -2,15 +2,28 @@ package LinkedList;
 
 public class Node<T> {
     protected Node<T> next;
+    protected Node<T> previous;
     private T element;
 
     protected Node(T element) {
         this.element = element;
     }
+
     @Override
     public String toString() {
-        return "Node{" +
-                "element=" + element +
-                '}';
+        String toReturn = "Node { next = ";
+        if(next == null) {
+            toReturn += "null";
+        } else {
+            toReturn += next.element;
+        }
+        toReturn += " | element = " + element + " | previous = ";
+        if(previous == null) {
+            toReturn += "null";
+        } else {
+            toReturn += previous.element;
+        }
+        toReturn += "}";
+        return toReturn;
     }
 }
