c9cf1214807b36c8e5d1b3b76d45958d0160d7c6
ViktorKrGo <viktor.krastev03@gmail.com>
2023-11-14 12:15:19 +0100

    Fixed PathFindNode, constructor items were in the wrong order.

--- src/PathFinding/Nodes/PathFindNode.java
+++ src/PathFinding/Nodes/PathFindNode.java
@@ -3,8 +3,8 @@ package PathFinding.Nodes;
 // TODO: Separated this and KruskalConnection because I couldn't make them use the same comparable.
 public class PathFindNode<T> extends BaseNode<T> implements Comparable<PathFindNode<T>> {
 
-    public PathFindNode(T destination, T origin, double cost) {
-        super(destination, origin, cost);
+    public PathFindNode(T origin, T destination, double cost) {
+        super(origin, destination, cost);
     }
 
     @Override
